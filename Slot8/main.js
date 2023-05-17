541bc4bff7dba4d2ac1ec4ff855b2dffaec0f81b
PhamDy <129242144+PhamDy@users.noreply.github.com>
2023-05-17 23:23:12 +0700

    Slot8 Creat
    Nhóm 3

---
+++ Slot8/main.js
@@ -0,0 +1,26 @@
+var app = angular.module("myApp", ['ngRoute']);
+
+app.config(function($routeProvider) {
+    $routeProvider 
+        // .when('/', {
+        //     controller: 'myCtrl',
+        //     templateUrl: 'home.html'
+        // }) 
+        .when('/userlist', {
+            templateUrl: 'views/tableContact.html'
+        })    
+});
+
+app.controller("myCtrl", function($scope, $http)
+{
+    $scope.contacts = [];
+    $scope.newContact = {};
+
+    $scope.addContact = function() {
+        $scope.contacts.push($scope.newContact);
+        $scope.newContact = {};
+        $scope.myForm.$setPristine();
+        alert("Successfully added, you can see the list by clicking User list");
+      };
+});
+
